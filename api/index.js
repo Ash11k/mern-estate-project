f5aeb43e98f07139e27908198f39025f36c9f7d8
Ash11k <koturwarashwini1@gmail.com>
2026-06-04 19:58:02 +0530

    fix render deployment

--- api/index.js
+++ api/index.js
@@ -25,11 +25,11 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,() => {
-    console.log('server is running on port 3000*');
-}
-);
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
